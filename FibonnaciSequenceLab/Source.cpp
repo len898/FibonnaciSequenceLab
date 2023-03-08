dc4518cbe87426b7431e2e679c0b03f91d5b5811
lennart@familie-richter.de <lennart@familie-richter.de>
2023-03-08 09:37:14 -0800

    Initial Commit

---
+++ FibonnaciSequenceLab/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int Fibonacci(int n) {
+
+	/* Type your code here. */
+
+}
+
+int main() {
+	int startNum;
+
+	cin >> startNum;
+	cout << "Fibonacci(" << startNum << ") is " << Fibonacci(startNum) << endl;
+
+	return 0;
+}
